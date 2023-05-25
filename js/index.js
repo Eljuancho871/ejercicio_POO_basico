4787369c22df13322b1a9ad747a381a7db3edce6
Juan Diego <juandiegoramirez098@gmail.com>
2023-05-25 15:06:37 -0500

    feat: :sparkles: ejercicio 5
    ejercicio 5 de POO, terminado

--- js/index.js
+++ js/index.js
@@ -1,68 +1,54 @@
 const $resultado1 = document.querySelector("#resultado1");
 const $resultado2 = document.querySelector("#resultado2");
 const $resultado3 = document.querySelector("#resultado3");
-class Figura {
+class Vehiculo {
 
-    constructor(color, area){
+    constructor(marca, modelo, velocidad){
 
-        this.color = color;
-        this.area = area;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidad = velocidad;
     }
 
-    calcularArea(){
+    acelerar(){
 
-        $resultado1.innerHTML = `El area es de ${this.area}`;
+        return this.velocidad += 10
+    }
+
+    static convertirKmHEnMph(velocidadK){
+
+        return velocidadK / 1.60934
+    }
+
+}
+
+class Coche extends Vehiculo {
+
+    constructor(combustible, marca, modelo, velocidad){
+
+        super(marca, modelo, velocidad);
+        this.combustible = combustible;
+    }
+
+    acelerar(){
+
+        return this.velocidad += 20;
     }
 }
 
-class Circulo extends Figura {
+
+document.querySelector("form"),addEventListener("submit", e => {
     
-    constructor(radio){
-        
-        super(null, null);
-        this.radio = radio;
-  } 
-  
-  calcularArea(){
-      
-      this.area = Math.PI * (this.radio * this.radio);
-      $resultado2.innerHTML = `El area del circulo es ${this.area}`;
-    }
-}
-
-class Rectangulo extends Figura {
-
-    constructor(largo, ancho){
-
-        super(null, null);
-        this.largo = largo;
-        this.ancho = ancho;
-    }
-
-    calcularArea(){
-
-      this.area = this.largo * this.ancho;
-      $resultado3.innerHTML = `El area del circulo es ${this.area}`;
-    }
-}
-
-
-document.querySelector("#form").addEventListener("submit", e => {
-
     e.preventDefault();
-    $color = document.querySelector("#color").value;
-    $area = document.querySelector("#area").value;
-    $radio = document.querySelector("#radio").value;
-    $largo = document.querySelector("#largo").value;
-    $ancho = document.querySelector("#ancho").value;
-
-    const figura1 = new Figura($color, $area);
-    figura1.calcularArea();
+    const $marca = document.querySelector("#marca").value;
+    const $modelo = document.querySelector("#modelo").value;
+    const $combustible = document.querySelector("#combustible").value;
+    const $velocidad = Number(document.querySelector("#velocidad").value);
+    const $velocidadC = Number(document.querySelector("#velocidadC").value);
     
-    const circculo1 = new Circulo($radio);
-    circculo1.calcularArea();
-    
-    const rectangulo1 = new Rectangulo($ancho, $largo);
-    rectangulo1.calcularArea();
+    const vehiculo1 = new Vehiculo($marca, $modelo, $velocidad);
+    $resultado1.innerHTML = vehiculo1.acelerar();
+    const coche1 = new Coche($combustible, $marca, $modelo, $velocidad);
+    $resultado2.innerHTML = coche1.acelerar();
+    $resultado3.innerHTML = Vehiculo.convertirKmHEnMph($velocidadC);
 })
-
