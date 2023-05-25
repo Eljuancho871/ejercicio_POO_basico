de96dd5b163bd9a932ca8656f276835b185256fc
Juan Diego <juandiegoramirez098@gmail.com>
2023-05-25 14:24:02 -0500

    feat: :sparkles: ejercicio 2
    ejercicio 2 de POO terminado

--- js/index.js
+++ js/index.js
@@ -1,44 +1,16 @@
 const $resultado1 = document.querySelector("#resultado1");
-const $resultado2 = document.querySelector("#resultado2");
-
 class Persona {
+    static esMayorEdad(edad){
 
-    constructor(nombre, edad, sexo){
-
-        this.nombre = nombre;
-        this.edad = edad;
-        this.sexo = sexo;
+        return edad >= 18 ? true : false;
     }
-
-    saludar(){
-
-        $resultado1.innerHTML = `holaaaa me llamo ${this.nombre}`;
-    }
-}
-
-class Estudiante extends Persona{
-
-    constructor(carrera, nombre, edad, sexo){
-        super(nombre, edad, sexo);
-        this.carrera = carrera;
-    }
-
-    estudiar(){
-        $resultado2.innerHTML = `y estudio ${this.carrera}`;
-    }
-
 }
 
 document.querySelector("#form").addEventListener("submit", e => {
 
     e.preventDefault();
-    const $nombre = document.querySelector("#nombre").value;
     const $edad = document.querySelector("#edad").value;
-    const $sexo = document.querySelector("#sexo").value;
-    const $carrera = document.querySelector("#carrera").value;
 
-    const estudiante1 = new Estudiante($carrera, $nombre, $edad, $sexo);
-    estudiante1.saludar();
-    estudiante1.estudiar();
+    $resultado1.innerHTML = Persona.esMayorEdad($edad);
     
 })
