0eebd0b070c3c35c5c39dff0516594a78a16760b
Juan Diego <juandiegoramirez098@gmail.com>
2023-05-25 14:28:12 -0500

    feat: :sparkles: ejercicio 3
    ejercicio 3 de POO, terminado

--- js/index.js
+++ js/index.js
@@ -1,16 +1,43 @@
 const $resultado1 = document.querySelector("#resultado1");
-class Persona {
-    static esMayorEdad(edad){
+const $resultado2 = document.querySelector("#resultado2");
+class Animal {
 
-        return edad >= 18 ? true : false;
+    constructor(nombre, edad){
+
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    hacerSonido(){
+
+        $resultado1.innerHTML = `El ${this.nombre} hace un sonido`;
+    }
+}
+
+class Perro extends Animal {
+
+    constructor(raza){
+        super();
+        this.raza = raza;
+    }
+
+    moverCola(){
+
+        $resultado2.innerHTML = "el perro esta moviendo la cola";
     }
 }
 
 document.querySelector("#form").addEventListener("submit", e => {
 
     e.preventDefault();
+    const $nombre = document.querySelector("#nombre").value;
     const $edad = document.querySelector("#edad").value;
+    const $raza = document.querySelector("#raza").value;
 
-    $resultado1.innerHTML = Persona.esMayorEdad($edad);
+    const animal1 = new Animal($nombre, $edad);
+    animal1.hacerSonido();
+
+    const perro1 = new Perro($raza);
+    perro1.moverCola();
     
 })
